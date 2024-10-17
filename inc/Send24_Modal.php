e3efacdd62a8b47f7a3ad1980d61c1833a18f8dd
progIfe <ifeoluwasamson90@gmail.com>
2024-10-17 14:33:52 +0100

    Update UI files

--- inc/Send24_Modal.php
+++ inc/Send24_Modal.php
@@ -20,59 +20,137 @@ class Send24_Modal {
 	}
 	private static function send24_modal($response){
 		echo '
-<div id="send24Modal" class="send24-modal" style="display: block;">
-	<div class="send24-modal-content">
-		<span id="closeSend24Modal" class="send24-close">&times;</span>
-		<img src="' . plugin_dir_url(__FILE__) . 'send24-logo.png" alt="Send24 Logo" class="send24-logo" />';
+		<div id="send24Modal" class="send24-modal" style="display: none;">
+			<div class="send24-modal-content">
+				<span id="closeSend24Modal" class="send24-close">&times;</span>
+				<img src="' . plugin_dir_url(__FILE__) . 'send24-logo.png" alt="Send24 Logo" class="send24-logo" />';
 
-		// Loop through the data to find HUB_TO_HUB and HUB_TO_DOOR
-		foreach ($response->data as $option) {
-			foreach ($option as $shipping_type => $details) {
-				if ($shipping_type === 'HUB_TO_DOOR' || $shipping_type === 'HUB_TO_HUB') {
-					$formatted_price = $details->formatted_price;
-					$price = $details->price;
-					$delivery_type = ($shipping_type === 'HUB_TO_DOOR') ? 'Door Delivery' : 'Hub Delivery';
-					$description = ($shipping_type === 'HUB_TO_HUB')
-						? 'Easily pick up your packages at a Send24 hub closest to you.'
-						: 'Have a Send24 agent deliver your packages to your doorstep.';
+				// Loop through the data to find HUB_TO_HUB and HUB_TO_DOOR
+				foreach ($response_data['data'] as $option) {
+				    foreach ($option as $shipping_type => $details) {
+				        if ($shipping_type === 'HUB_TO_DOOR' || $shipping_type === 'HUB_TO_HUB') {
+				            $formatted_price = $details['formatted_price'];
+				            $price = $details['price'];
+				            $delivery_type = ($shipping_type === 'HUB_TO_DOOR') ? 'Door Delivery' : 'Hub Delivery';
+				            $description = ($shipping_type === 'HUB_TO_HUB')
+				                ? 'Easily pick up your packages at a Send24 hub closest to you.'
+				                : 'Have a Send24 agent deliver your packages to your doorstep.';
+								
+				            // Display each option in a card-like structure
+				            echo "
+				            <div class='send24-shipping-option'>
+				                <label class='send24-shipping-label'>
+				                    <input type='radio' name='send24_shipping_option' data-price='$price' value='$shipping_type'>
+				                    $delivery_type
+				                </label>
+				                <span class='send24-shipping-price'>₦$formatted_price</span>
+				                <p class='send24-description'>$description</p>";
+								
+				            // Button for HUB_TO_HUB shipping type
+				            if ($shipping_type === 'HUB_TO_HUB') {
+				                echo "
+				                <h4 class='send24-hub-text'>
+				                    <a href='#' id='toggleHubsLinkclass='send24-hub-link'>
+        								<i>See Nearby Hubs</i>
+        							</a>
+				                </h4>
 
-					// Display each option in a card-like structure
-					echo "
-		<div class='send24-shipping-option'>
-			<label class='send24-shipping-label'>
-				<input type='radio' name='send24_shipping_option' data-price='$price' value='$shipping_type'>
-				$delivery_type
-			</label>
-			<span class='send24-shipping-price'>₦$formatted_price</span>
-			<p class='send24-description'>$description</p>";
+								<!-- Nearby Hubs List -->
+                				<div id='nearbyHubsList' style='display:none; max-height: 200px; overflow-y: auto; border: 1px solid #ccc; padding: 10px; margin-top: 10px;'>
+                    				<form id='hubSelectionForm'>
+                    				<ul style='list-style: none; padding-left: 0;'>";
 
-					// Button for HUB_TO_HUB shipping type
-					if ($shipping_type === 'HUB_TO_HUB') {
-						echo "
-			<h4 class='send24-hub-text'>
-				<a href='https://send24.co/hubs' class='send24-hub-link' target=blank>
-					<i>See Nearby Hubs</i>
-				</a>
-			</h4>";
-					}
+                				// Loop through the recommended hubs
+                				foreach ($details['recommended_hubs'] as $hub) {
+                					$hub_name = $hub['name'];
+                					$hub_address = $hub['address'];
+                					$hub_phone = $hub['phone'];
+                					$hub_distance = $hub['distance'];
+									$hub_uuid = $hub['uuid'];
 
-					echo "</div>";
+                					echo "
+                    				<li style='border-bottom: 1px solid #ddd; padding: 10px 0; display: flex; align-items: center;'>
+            							<label style='cursor: pointer; display: flex; align-items: center;'>
+            								<input type='radio' name='selected_hub' value='$hub_uuid' style='margin-right: 10px;' onclick='highlightHub(this)'>
+            								<div class='send24-description' style='display: inline-block;'>
+            								    <strong>$hub_name</strong><br>
+            								    Address: $hub_address<br>
+            								    Distance from your Location{$hub_distance}km<br>
+            								    Phone Number: $hub_phone
+            								</div>
+            							</label>
+            						</li>";
+                				}
+
+                				echo "
+                					</ul>
+									</form>
+                				</div>";
+				            }
+								
+				            echo "</div>";
+				        }
+				    }
 				}
-			}
-		}
 
-		echo '
-		
-		<div class="send24-confirm">
-			<button id="confirmSend24Shipping" class="send24-confirm-button">
-				<span class="button-text">Confirm Shipping Option</span>
-				<span class="button-loader" style="display:none;">
-        						    <div class="loader"></div>
-        						</span>
-			</button>
-		</div>
-	</div>
-</div>
+				echo "
+				<script>
+				    document.getElementById('toggleHubsLink').addEventListener('click', function(event) {
+				        event.preventDefault();
+				        var hubDeliveryRadio = document.querySelector('input[name=send24_shipping_option][value=HUB_TO_HUB]');
+
+				        // Check if the Hub Delivery radio button is checked
+				        if (!hubDeliveryRadio.checked) {
+				            alert('Please select Hub Delivery first to see nearby hubs.');
+				            return;
+				        }
+
+				        var hubsList = document.getElementById('nearbyHubsList');
+				        if (hubsList.style.display === 'none') {
+				            hubsList.style.display = 'block';
+				        } else {
+				            hubsList.style.display = 'none';
+				        }
+				    });
+
+
+				function highlightHub(selectedHub) {
+					var hubs = document.getElementsByName('selected_hub');
+					hubs.forEach(function(hub) {
+						hub.closest('li').style.backgroundColor = '#fff';
+					});
+
+					// Highlight the selected hub
+					selectedHub.closest('li').style.backgroundColor = '#F5F2DC';
+				}
+
+					document.querySelectorAll('input[name=send24_shipping_option]').forEach(function(radio) {
+                	radio.addEventListener('change', function() {
+            		    var hubsList = document.getElementById('nearbyHubsList');
+            		    if (this.value === 'HUB_TO_DOOR' && hubsList.style.display === 'block') {
+            		        hubsList.style.display = 'none';
+            		    }
+            		});
+            	});
+				</script>";
+					
+			echo '
+				<!-- Confirm Button -->
+				<div class="send24-confirm">
+				    <button id="confirmSend24Shipping" class="send24-confirm-button">
+				        <span class="button-text">Confirm Shipping Option</span>
+        				<span class="button-loader" style="display:none;">
+        					<div class="loader"></div>
+        				</span>
+				    </button>
+				</div>
+			</div>
+		</div>';
+
+	
+			// } else {
+			// 	echo '<p>Unable to fetch Send24 shipping options at this time. Please try again later.</p>';
+			// }
 
 
 <!-- Modal Script -->
